bb32dc1c2557cc69c1ec494822902e807c7067f6
Umer Sikander <90478768+Umer-450@users.noreply.github.com>
2024-11-02 02:46:44 -0400

    Bulk upload 4-1 to 4-2

---
+++ assignment4-1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace assignment4_1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Question: 1. Write a program to produce a table of numbers from 10 to 1,
+        // with their squares and cubes. Again, your table must look professional!
+
+        // Table Header
+        Console.WriteLine("Number\tSquare\tCube");
+        Console.WriteLine("------\t------\t----");
+
+        // Loop through numbers 10 - 1
+        for (int counter = 10; counter >= 1; counter--)
+        {
+            int square = counter * counter;
+            int cube = counter * counter * counter;
+
+            // Print each row
+            Console.WriteLine($"{counter}\t{square}\t{cube}");
+        }
+    }
+}
