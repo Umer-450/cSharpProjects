eb91d50eebb4d666c572f10abfa76cee9e774402
Umer Sikander <90478768+Umer-450@users.noreply.github.com>
2024-11-04 23:39:37 -0500

    Bulk upload for projects 4-3 and 4-4

---
+++ assignment4-3/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace assignment4_3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Question: 3. Write and run a C# program that calculates and displays
+        // the amount of money available in a bank account that initially has
+        // $1,000 deposited in it and that earns 8 percent interest a year.
+        // Your program should display the amount available at the end of each
+        // year for a period of ten years. Use the relationship that the money
+        // available at the end of each year equals the amount of money in the
+        // account at the start of the year plus .08 times the amount available
+        // at the start of the year.
+
+        // Declare variables
+        double balance = 1000;
+        double interest = 0.08;
+
+        // Table header
+        Console.WriteLine("\nYear\tBalance");
+
+        // Loop to display yearly balance
+        for (int year = 1; year <= 10; year++)
+        {
+            // Inner while loop to run once per year (per iteration)
+            int counter = 0;
+            while (counter < 1)
+            {
+                balance += balance * interest;
+                counter++;
+            }
+            // Display output
+            Console.WriteLine($"{year}\t{balance:C}");
+        }
+    }
+}
---
+++ assignment4-4/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace assignment4_4;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Question 4. Write a C program that continuously requests a grade.
+        // If the grade is less than 0 or greater than 100,
+        // your program should print an appropriate message informing the user
+        // that an invalid grade has been entered, else the grade should be added
+        // to a total.
+        // If the grade is 999, the program should exit the loop and display
+        // the sum, number of valid grades and the average of the valid grades
+        // entered. (You must NOT display an invalid message)
+        // P.S.The value 999 is use to terminate the cycle and should not be used
+        // in any of the calculation. You must not display an “Invalid grade"
+
+        Console.WriteLine("Enter grades one by one (Enter '999' to finish):");
+
+        // Declare variables
+        int total = 0;
+        int count = 0;
+
+        // Processing
+        for (int grade = 0; grade != 999;)
+        {
+            // Prompt user for grade
+            Console.WriteLine("Enter a grade:");
+            grade = Convert.ToInt32(Console.ReadLine());
+
+            // Stop the loop if user enters 999
+            if (grade == 999)
+            {
+                break;
+            }
+
+            // while loop to validate the grade
+            while (grade < 0 || grade > 100)
+            {
+                Console.WriteLine("Invalid grade! Please enter a grade from 0 to 100:");
+                grade = Convert.ToInt32(Console.ReadLine());
+            }
+
+            total += grade;
+            count++;
+
+            continue;
+        }
+
+        // Calculate the average
+        double average = (double)total / count;
+
+        // Display the results
+        Console.WriteLine($"\nThe sum of your grades is {total}.");
+        Console.WriteLine($"The number of valid grades you entered is {count}.");
+        Console.WriteLine($"Your average grade is {average}.");
+    }
+}
