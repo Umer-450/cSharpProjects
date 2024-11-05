4b1d65c34d23fb4c3d139ff6383337010e8e42b5
Umer Sikander <90478768+Umer-450@users.noreply.github.com>
2024-11-04 23:38:49 -0500

    Adding alternate solutions for 4-1 and 4-2

---
+++ assignment4-1_while/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace assignment4_1_while;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Question: 1. Write a program to produce a table of numbers from 10 to 1,
+        // with their squares and cubes. Again, your table must look professional!
+
+        // Table Header
+        Console.WriteLine("Number\tSquare\tCube");
+        Console.WriteLine("------\t------\t----");
+
+        int counter = 10;
+
+        // Loop through numbers 10 - 1
+        while (counter >= 1)
+        {
+            int square = counter * counter;
+            int cube = counter * counter * counter;
+
+            // Print each row
+            Console.WriteLine($"{counter}\t{square}\t{cube}");
+            counter--;
+        }
+    }
+}
---
+++ assignment4-2_for/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace assignment4_2_for;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Question: 2. Write a program that displays a conversion table
+        // from Fahrenheit to Celsius. The program must request the starting
+        // Fahrenheit value, the ending Fahrenheit value, and the increment.
+        // Thus, instead of the condition checking for a fixed count,
+        // the condition checks for the ending Fahrenheit value.
+        // Your table must look professional!
+
+        // Prompt user for inputs
+        Console.WriteLine("Enter the starting Fahrenheit value");
+        int startValue = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the ending Fahrenheit value");
+        int endValue = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the increment value");
+        int increment = Convert.ToInt32(Console.ReadLine());
+
+        // Table header
+        Console.WriteLine("\nFahrenheit\tCelsius");
+        Console.WriteLine("__________\t_______");
+
+        // Loop
+        for (; startValue <= endValue; startValue += increment)
+        {
+            double celsius = (startValue - 32) * 5.0 / 9.0;
+
+            Console.WriteLine($"{startValue,10}\t{celsius,7:F2}");
+        }
+    }
+}
